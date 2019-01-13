50911abed647d007d0cd458ea2cd051d9bf40d28
Markus Kremer <mkremer@seibert-media.net>
2019-01-13 23:04:22 +0100

    added keyboard controls + code cleanup + structure

--- brick-breaker.js
+++ brick-breaker.js
@@ -1,320 +1,109 @@
 document.addEventListener('DOMContentLoaded', () => {
-	let canvas = document.querySelector('#canvas');
-	let viewport = {w: window.innerWidth, h: window.innerHeight};
-	let ctx = canvas.getContext('2d');
-	let game = {
-		running: false,
-		hideCursor: true,
-		devOrb: null,
-		statsHud: null,
-		playerBoard: null,
-		brickArea: null,
-		orbs: [],
-		bricks: [],
-		gridSegments: []
-	};
 
-	window.expose = {
-		renderGrid: game.devRenderGrid,
-		getGame: () => {return game}
-	};
+// VARS AND STUFF ============================================================
+let canvas = document.querySelector('#canvas');
+let viewport = {w: window.innerWidth, h: window.innerHeight};
+let ctx = canvas.getContext('2d');
+let game = {
+	running: false,
+	boardControl: {left: false, right: false, top: false, bottom: false},
+	devOrb: null,
+	statsHud: null,
+	playerBoard: null,
+	brickArea: null,
+	orbs: [],
+	bricks: [],
+	gridSegments: []
+};
 
-	document.addEventListener('mousemove', e => {
-		if (game.running) {
-			game.playerBoard.move(e);
-		}
-		// game.devOrb.move(e);
-	});
-	document.addEventListener('keypress', e => {
-		if (e.keyCode === 32 && !game.running) {
-			game.running = true;
-		}
-	});
+// EXPOSE (deal game obj to console) ============================================================
+window.expose = {
+	getGame: () => {return game}
+};
 
-	canvas.width = viewport.w;
-	canvas.height = viewport.h;
-	if (game.hideCursor) canvas.style.cursor = 'none';
+// SET CANVAS SIZE ============================================================
+canvas.width = viewport.w;
+canvas.height = viewport.h;
+
+// EVENT LISTENER ============================================================
+// keyCodes: 65 = A // 68 = D // 87 = W // 83 = S // 32 = SPACE
+document.addEventListener('mousemove', e => {
+	// game.devOrb.move(e);
+});
+document.addEventListener('keydown', e => {
+	// spacebar to launch orb from board (and start the game)
+	if (e.keyCode === 32 && !game.running) {
+		game.running = true;
+	}
+	if (e.keyCode === 68) {
+		game.boardControl.right = true;
+		game.boardControl.left = false;
+	}
+	if (e.keyCode === 65) {
+		game.boardControl.left = true;
+		game.boardControl.right = false;
+	}
+});
+document.addEventListener('keyup', e => {
+	if (e.keyCode === 68) game.boardControl.right = false;
+	if (e.keyCode === 65) game.boardControl.left = false;
+});
 
 
-	const InitCanvas = () => {
-		ctx.fillStyle = '#161618';
-		ctx.fillRect(0, 0, viewport.w, viewport.h);
+// FUNCTIONS ============================================================
+const InitCanvas = () => {
+	ctx.fillStyle = '#161618';
+	ctx.fillRect(0, 0, viewport.w, viewport.h);
+}
+
+const Init = () => {
+	// setup
+	game.brickArea = new BrickArea(viewport.w / 100 * 5, 50, viewport.w / 100 * 90, viewport.h / 2, false);
+	game.statsHud = new StatsHud();
+	game.playerBoard = new PlayerBoard();
+	game.orbs.push(new Orb(game.playerBoard.x, game.playerBoard.y - 10));
+	// game.devOrb = new DevOrb();
+
+	// TEMP level generator
+	offsetX = game.brickArea.x + 2.5;
+	for (let i = 0; i < 10; i++) {
+		game.bricks.push(new Brick(offsetX, game.brickArea.y, 'brown'));
+		offsetX += game.brickArea.w / 10;
 	}
 
-	const Init = () => {
-		game.brickArea = new BrickArea(viewport.w / 100 * 5, 50, viewport.w / 100 * 90, viewport.h / 2, false);
-		game.statsHud = new StatsHud();
-		game.playerBoard = new PlayerBoard();
-		game.orbs.push(new Orb(game.playerBoard.x, game.playerBoard.y - 10));
-		// game.devOrb = new DevOrb();
-
-		// TEMP: lvl generator
-		offsetX = game.brickArea.x + 2.5;
-		for (let i = 0; i < 10; i++) {
-			game.bricks.push(new Brick(offsetX, game.brickArea.y, 'brown'));
-			offsetX += game.brickArea.w / 10;
-		}
-
-		offsetX = game.brickArea.x + 2.5 + game.brickArea.w / 10 - (game.brickArea.w / 10) / 2;
-		for (let i = 0; i < 9; i++) {
-			game.bricks.push(new Brick(offsetX, game.brickArea.y + 30, 'brown'));
-			offsetX += game.brickArea.w / 10;
-		}
-
-		offsetX = game.brickArea.x + 2.5 + game.brickArea.w / 10;
-		for (let i = 0; i < 8; i++) {
-			game.bricks.push(new Brick(offsetX, game.brickArea.y + 60, 'brown'));
-			offsetX += game.brickArea.w / 10;
-		}
-
-		offsetX = game.brickArea.x + 2.5 + game.brickArea.w / 10 + (game.brickArea.w / 10) / 2;
-		for (let i = 0; i < 7; i++) {
-			game.bricks.push(new Brick(offsetX, game.brickArea.y + 90, 'brown'));
-			offsetX += game.brickArea.w / 10;
-		}
-
-		console.log(game);
-		Update();
+	offsetX = game.brickArea.x + 2.5 + game.brickArea.w / 10 - (game.brickArea.w / 10) / 2;
+	for (let i = 0; i < 9; i++) {
+		game.bricks.push(new Brick(offsetX, game.brickArea.y + 30, 'brown'));
+		offsetX += game.brickArea.w / 10;
 	}
 
-	const Update = () => {
-		InitCanvas();
-
-		if (game.brickArea.renderGrid) {
-			game.brickArea.draw();
-			let segments = game.gridSegments.length;
-			for (let i = 0; i < segments; i++) {
-				game.gridSegments[i].draw();
-			}
-		}
-
-		// Ruler();
-
-		game.statsHud.draw();
-		game.playerBoard.draw();
-		// game.devOrb.isColliding();
-		// game.devOrb.draw();
-
-		let bricks = game.bricks.length;
-		for (let i = 0; i < bricks; i++) {
-			if (!game.bricks[i].hidden) {
-				game.bricks[i].draw();
-			}
-		}
-
-		for (let i = 0; i < game.orbs.length; i++) {
-			if (game.running) {
-				game.orbs[i].isColliding();
-				game.orbs[i].y += game.orbs[i].vy;
-				game.orbs[i].x += game.orbs[i].vx;
-			}
-			game.orbs[i].draw();
-		}
-
-		requestAnimationFrame(Update);
+	offsetX = game.brickArea.x + 2.5 + game.brickArea.w / 10;
+	for (let i = 0; i < 8; i++) {
+		game.bricks.push(new Brick(offsetX, game.brickArea.y + 60, 'brown'));
+		offsetX += game.brickArea.w / 10;
 	}
 
-	class BrickArea {
-		constructor(x, y, w, h, renderGrid = false) {
-			this.x = x;
-			this.y = y;
-			this.w = w;
-			this.h = h;
-			this.renderGrid = renderGrid;
-			this.segmentsPerRow = 4;// grid
-
-			// generate grid
-			let offsetX = this.x;
-			let offsetY = this.y;
-			let col = 1;// "next line" -controller
-			for (let i = 0; i < this.segmentsPerRow * this.segmentsPerRow; i++) {
-				// the object parameter contains width and height of THIS (= BrickArea).
-				game.gridSegments.push(new GridSegment(offsetX, offsetY, this.segmentsPerRow, i, {w: this.w, h: this.h}));
-				offsetX += this.w / this.segmentsPerRow;
-				if (col === this.segmentsPerRow) {
-					offsetX = this.x;// initial value
-					offsetY += this.h / this.segmentsPerRow;
-					col = 0;
-				}
-				col++;
-			}
-
-			this.draw = () => {
-				ctx.fillStyle = "rgba(0, 255, 0, 0.1)";
-				ctx.fillRect(game.brickArea.x, game.brickArea.y, game.brickArea.w, game.brickArea.h);
-				ctx.fill();
-			}
-		}
+	offsetX = game.brickArea.x + 2.5 + game.brickArea.w / 10 + (game.brickArea.w / 10) / 2;
+	for (let i = 0; i < 7; i++) {
+		game.bricks.push(new Brick(offsetX, game.brickArea.y + 90, 'brown'));
+		offsetX += game.brickArea.w / 10;
 	}
 
-	class GridSegment {
-		constructor(x, y, size, id, brickAreaSize) {
-			this.x = x;
-			this.y = y;
-			this.w = brickAreaSize.w / size;
-			this.h = brickAreaSize.h / size;
-			this.color = 'rgba(0, 255, 255, 0.5)';
-			this.id = id;
-			this.contains = [];
+	// console.log(game);
+	Update();
+}
 
-			this.draw = () => {
-				ctx.strokeStyle = this.color;
-				ctx.strokeRect(this.x, this.y, this.w, this.h);
-				ctx.fillText(this.id, this.x + this.w / 2.2, this.y + this.h / 1.6);
-			}
+const Update = () => {
+	InitCanvas();
+
+	// whether to render grid or not
+	if (game.brickArea.renderGrid) {
+		game.brickArea.draw();
+		let segments = game.gridSegments.length;
+		for (let i = 0; i < segments; i++) {
+			game.gridSegments[i].draw();
 		}
-	}
-
-	class StatsHud {
-		constructor() {
-			this.x = 5;
-			this.y = 5;
-			this.w = viewport.w / 100 * 25;
-			this.h = 35;
-
-			this.draw = () => {
-				ctx.fillStyle = 'rgba(255, 255, 255, 0.25)';
-				ctx.fillRect(this.x, this.y, this.w, this.h);
-				ctx.fill();
-
-				ctx.font = "16px Arial";
-				ctx.fillText("Time: 0s // Score: 0", 10, this.h / 1.3);
-
-				// top wall
-				ctx.fillStyle = 'rgba(255, 255, 255, 0.1)';
-				ctx.fillRect(0, 0, viewport.w, game.brickArea.y);
-				ctx.fill();
-			}
-		}
-	}
-
-	class Brick {
-		constructor(x, y, color) {
-			this.x = x;
-			this.y = y;
-			this.w = game.brickArea.w / 15 - 5;
-			this.h = 25;
-			this.fillColor = color;
-			this.hidden = false;
-
-			this.within = segment => {
-				if ((this.x + this.w) >= segment.x && this.x <= (segment.x + segment.w) && (this.y + this.h) >= segment.y && this.y <= (segment.y + segment.h)) {
-					return true;
-				}
-			}
-
-			// segment check
-			for (let i = 0; i < game.gridSegments.length; i++) {
-				if (this.within(game.gridSegments[i])) {
-					game.gridSegments[i].contains.push(this);
-				}
-			}
-
-			this.draw = () => {
-				ctx.fillStyle = this.fillColor;
-				ctx.fillRect(this.x, this.y, this.w, this.h);
-				ctx.fill();
-			}
-		}
-	}
-
-	class Orb {
-		constructor(x, y) {
-			this.x = x;
-			this.y = y;
-			this.vx = Math.floor(Math.random() * 5 + 3);
-			this.vy = 1;
-			this.radius = 6;
-			this.fillColor = 'cyan';
-
-			this.hits = (obj) => {
-				return (this.x + this.radius) >= obj.x && this.x + this.radius <= (obj.x + obj.w + this.radius * 2) && (this.y + this.radius) >= obj.y && this.y + this.radius <= (obj.y + obj.h + this.radius * 2);
-			}
-
-			this.isColliding = () => {
-				if (this.y - this.radius <= game.brickArea.y) this.vy *= -1;// top
-				let segments = game.gridSegments;
-				for (let i = 0; i < segments.length; i++) {
-					if (this.hits(segments[i])) {
-						segments[i].color = 'rgb(0, 255, 0)';
-						for (let j = 0; j < segments[i].contains.length; j++) {
-							// segments[i].contains[j].fillColor = 'dodgerblue';
-							if (!segments[i].contains[j].hidden && this.hits(segments[i].contains[j])) {
-								// segments[i].contains[j].fillColor = 'red';
-								segments[i].contains[j].hidden = true;
-								this.vy *= -1;
-							}
-						}
-					} else {
-						segments[i].color = 'rgba(0, 255, 255, 0.5)';
-					}
-
-				}
-
-				// check for collisions if outside brick area
-				if (this.x - this.radius <= 0) this.vx *= -1;// left wall
-				if (this.x + this.radius >= viewport.w) this.vx *= -1;// right wall
-
-				let distX = (this.x + this.radius) - (game.playerBoard.x - game.playerBoard.w / 2);
-				let distY = (this.y + this.radius) - game.playerBoard.y;
-				if ((distX >= 0 && distX <= game.playerBoard.w + this.radius * 2) && (distY >= 0 && distY <= game.playerBoard.h + this.radius * 2)) {
-					this.vy = Math.floor(Math.random() * 5 + 5) * -1;
-					// this.vx = Math.floor(Math.random() * 5 + 5) * -1;
-				}
-			}
-
-			this.draw = () => {
-				ctx.fillStyle = this.fillColor;
-				ctx.beginPath();
-				ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-				ctx.fill();
-
-				// ctx.font = "14px Arial";
-				// ctx.fillText('x:'+this.x.toFixed(0)+' y:'+this.y.toFixed(0), this.x + this.radius + 10, this.y);
-			}
-		}
-	}
-
-	class DevOrb extends Orb {
-		constructor() {
-			super(500, 500);
-			this.vx = 0;
-			this.vy = 0;
-
-			this.move = event => {
-				this.x = event.pageX;
-				this.y = event.pageY;
-			}
-		}
-	}
-
-	class PlayerBoard {
-		constructor() {
-			this.x = viewport.w / 2;
-			this.y = viewport.h - 25;
-			this.vx = 50;
-			this.vy = 10;
-			this.w = viewport.w / 100 * 10;
-			this.h = 7;
-			this.fillColor = '#fff';
-
-			this.move = event => {
-				this.x = event.pageX;
-			}
-
-			this.draw = () => {
-				ctx.fillStyle = this.fillColor;
-				ctx.fillRect(this.x - this.w / 2, this.y, this.w, this.h);
-				ctx.fill();
-
-				// ctx.font = "14px Arial";
-				// ctx.fillText('x:'+this.x.toFixed(0)+' y:'+this.y.toFixed(0), this.x + this.w / 2 + 10, this.y);
-			}
-		}
-	}
-
-
-	const Ruler = () => {
+		// ruler
 		ctx.strokeStyle = 'darkred';
 		ctx.beginPath();
 		ctx.moveTo(viewport.w / 2, 0);
@@ -327,6 +116,242 @@ document.addEventListener('DOMContentLoaded', () => {
 		ctx.stroke();
 	}
 
+	// player board movement
+	if (game.boardControl.left) game.playerBoard.x -= game.playerBoard.vx;
+	if (game.boardControl.right) game.playerBoard.x += game.playerBoard.vx;
 
-	Init();
+	// GAME COMPONENTS
+	game.statsHud.draw();
+	game.playerBoard.draw();
+	// game.devOrb.isColliding();
+	// game.devOrb.draw();
+
+	// BRICKS
+	let bricks = game.bricks.length;
+	for (let i = 0; i < bricks; i++) {
+		if (!game.bricks[i].hidden) {
+			game.bricks[i].draw();
+		}
+	}
+
+	// ORBS
+	for (let i = 0; i < game.orbs.length; i++) {
+		if (game.running) {
+			game.orbs[i].isColliding();
+			game.orbs[i].x += game.orbs[i].vx;
+			game.orbs[i].y += game.orbs[i].vy;
+		} else {
+			game.orbs[i].x = game.playerBoard.x;
+			game.orbs[i].y = game.playerBoard.y - 10;
+		}
+		game.orbs[i].draw();
+	}
+
+	requestAnimationFrame(Update);
+}
+
+
+// CLASSES ============================================================
+class BrickArea {
+	constructor(x, y, w, h, renderGrid = false) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.renderGrid = renderGrid;
+		this.segmentsPerRow = 4;// grid
+
+		// generate grid
+		let offsetX = this.x;
+		let offsetY = this.y;
+		let col = 1;// "next line" -controller
+		for (let i = 0; i < this.segmentsPerRow * this.segmentsPerRow; i++) {
+			// the object parameter contains width and height of THIS (= BrickArea).
+			game.gridSegments.push(new GridSegment(offsetX, offsetY, this.segmentsPerRow, i, {w: this.w, h: this.h}));
+			offsetX += this.w / this.segmentsPerRow;
+			if (col === this.segmentsPerRow) {
+				offsetX = this.x;// initial value
+				offsetY += this.h / this.segmentsPerRow;
+				col = 0;
+			}
+			col++;
+		}
+
+		this.draw = () => {
+			ctx.fillStyle = "rgba(0, 255, 0, 0.1)";
+			ctx.fillRect(game.brickArea.x, game.brickArea.y, game.brickArea.w, game.brickArea.h);
+			ctx.fill();
+		}
+	}
+}
+
+class GridSegment {
+	constructor(x, y, size, id, brickAreaSize) {
+		this.x = x;
+		this.y = y;
+		this.w = brickAreaSize.w / size;
+		this.h = brickAreaSize.h / size;
+		this.color = 'rgba(0, 255, 255, 0.5)';
+		this.id = id;
+		this.contains = [];
+
+		this.draw = () => {
+			ctx.strokeStyle = this.color;
+			ctx.strokeRect(this.x, this.y, this.w, this.h);
+			ctx.fillText(this.id, this.x + this.w / 2.2, this.y + this.h / 1.6);
+		}
+	}
+}
+
+class StatsHud {
+	constructor() {
+		this.x = 5;
+		this.y = 5;
+		this.w = viewport.w / 100 * 25;
+		this.h = 35;
+
+		this.draw = () => {
+			ctx.fillStyle = 'rgba(255, 255, 255, 0.25)';
+			ctx.fillRect(this.x, this.y, this.w, this.h);
+			ctx.fill();
+
+			ctx.font = "16px Arial";
+			ctx.fillText("Time: 0s // Score: 0", 10, this.h / 1.3);
+
+			// top wall
+			ctx.fillStyle = 'rgba(255, 255, 255, 0.1)';
+			ctx.fillRect(0, 0, viewport.w, game.brickArea.y);
+			ctx.fill();
+		}
+	}
+}
+
+class Brick {
+	constructor(x, y, color) {
+		this.x = x;
+		this.y = y;
+		this.w = game.brickArea.w / 15 - 5;
+		this.h = 25;
+		this.fillColor = color;
+		this.hidden = false;
+
+		this.within = segment => {
+			if ((this.x + this.w) >= segment.x && this.x <= (segment.x + segment.w) && (this.y + this.h) >= segment.y && this.y <= (segment.y + segment.h)) {
+				return true;
+			}
+		}
+
+		// segment check
+		for (let i = 0; i < game.gridSegments.length; i++) {
+			if (this.within(game.gridSegments[i])) {
+				game.gridSegments[i].contains.push(this);
+			}
+		}
+
+		this.draw = () => {
+			ctx.fillStyle = this.fillColor;
+			ctx.fillRect(this.x, this.y, this.w, this.h);
+			ctx.fill();
+		}
+	}
+}
+
+class Orb {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.vx = Math.floor(Math.random() * 5 + 3);
+		this.vy = 1;
+		this.radius = 6;
+		this.fillColor = 'cyan';
+
+		this.hits = (obj) => {
+			return (this.x + this.radius) >= obj.x && this.x + this.radius <= (obj.x + obj.w + this.radius * 2) && (this.y + this.radius) >= obj.y && this.y + this.radius <= (obj.y + obj.h + this.radius * 2);
+		}
+
+		this.isColliding = () => {
+			if (this.y - this.radius <= game.brickArea.y) this.vy *= -1;// top
+			let segments = game.gridSegments;
+			for (let i = 0; i < segments.length; i++) {
+				if (this.hits(segments[i])) {
+					segments[i].color = 'rgb(0, 255, 0)';
+					for (let j = 0; j < segments[i].contains.length; j++) {
+						// segments[i].contains[j].fillColor = 'dodgerblue';
+						if (!segments[i].contains[j].hidden && this.hits(segments[i].contains[j])) {
+							// segments[i].contains[j].fillColor = 'red';
+							segments[i].contains[j].hidden = true;
+							this.vy *= -1;
+						}
+					}
+				} else {
+					segments[i].color = 'rgba(0, 255, 255, 0.5)';
+				}
+
+			}
+
+			// check for collisions if outside brick area
+			if (this.x - this.radius <= 0) this.vx *= -1;// left wall
+			if (this.x + this.radius >= viewport.w) this.vx *= -1;// right wall
+
+			let distX = (this.x + this.radius) - (game.playerBoard.x - game.playerBoard.w / 2);
+			let distY = (this.y + this.radius) - game.playerBoard.y;
+			if ((distX >= 0 && distX <= game.playerBoard.w + this.radius * 2) && (distY >= 0 && distY <= game.playerBoard.h + this.radius * 2)) {
+				this.vy = Math.floor(Math.random() * 5 + 5) * -1;
+				// this.vx = Math.floor(Math.random() * 5 + 5) * -1;
+			}
+		}
+
+		this.draw = () => {
+			ctx.fillStyle = this.fillColor;
+			ctx.beginPath();
+			ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+			ctx.fill();
+
+			// ctx.font = "14px Arial";
+			// ctx.fillText('x:'+this.x.toFixed(0)+' y:'+this.y.toFixed(0), this.x + this.radius + 10, this.y);
+		}
+	}
+}
+
+class PlayerBoard {
+	constructor() {
+		this.x = viewport.w / 2;
+		this.y = viewport.h - 25;
+		this.vx = 15;
+		this.vy = 5;
+		this.w = viewport.w / 100 * 10;
+		this.h = 7;
+		this.fillColor = '#fff';
+
+		this.move = event => {
+			this.x = event.pageX;
+		}
+
+		this.draw = () => {
+			ctx.fillStyle = this.fillColor;
+			ctx.fillRect(this.x - this.w / 2, this.y, this.w, this.h);
+			ctx.fill();
+
+			// ctx.font = "14px Arial";
+			// ctx.fillText('x:'+this.x.toFixed(0)+' y:'+this.y.toFixed(0), this.x + this.w / 2 + 10, this.y);
+		}
+	}
+}
+
+class DevOrb extends Orb {
+	constructor() {
+		super(500, 500);
+		this.vx = 0;
+		this.vy = 0;
+
+		this.move = event => {
+			this.x = event.pageX;
+			this.y = event.pageY;
+		}
+	}
+}
+
+
+Init();
+
 });
