1b4fc99b67bd2c9490e55bc3d2f82c20f40e0249
Markus Kremer <mkremer@seibert-media.net>
2019-01-17 19:09:07 +0100

    improved level 1 code

--- game/modules/level/level-1.js
+++ game/modules/level/level-1.js
@@ -1,21 +1,22 @@
 const level_1 = () => {
-	let blocksPerRow, offsetX = 0;
-	let center = (blocksPerRow) => {
+	let rowCount = 0, offsetX = 0;
+	let center = blocksPerRow => {
 		return ((blocksPerRow * 60) - viewport.w) / 2 * -1;
 	}
 
-
-	const createRow = (blocksPerRow, rowNumber) => {
+	const createRow = blocksPerRow => {
 		for (var i = 0; i < blocksPerRow; i++) {
 			offsetX = (i * 60) + center(blocksPerRow);
-			game.bricks.push(new Brick(offsetX, game.brickArea.y + (rowNumber * 43)));
+			game.bricks.push(new Brick(offsetX, game.brickArea.y + (rowCount * 43)));
 		}
+		rowCount++;
 	}
 
-	createRow(20, 0);
-	createRow(10, 1);
-	createRow(15, 2);
-	createRow(2, 3);
-	createRow(11, 4);
-	createRow(6, 5);
+	createRow(20);
+	createRow(10);
+	createRow(15);
+	createRow(2);
+	createRow(11);
+	createRow(4);
+	createRow(6);
 }
