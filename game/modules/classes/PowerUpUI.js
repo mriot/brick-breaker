d6d488b6524272b7a4f8000086db9c30ffe3fdd7
Markus Kremer <mkremer@seibert-media.net>
2019-01-18 12:14:04 +0100

    implemented powerup lifespan timer

--- game/modules/classes/PowerUpUI.js
+++ game/modules/classes/PowerUpUI.js
@@ -2,15 +2,29 @@ class PowerUpUI {
 	constructor() {
 		this.x = viewport.w - 40;
 		this.y = 0;
-		this.w = 40;
-		this.h = 40;
+		this.w = viewport.w;
+		this.h = 5;
+		this.powerUpLifetime = 0;
+		this.initLifetime = 0;
+
+		this.timer = lifetime => {
+			this.powerUpLifetime = this.initLifetime = lifetime;
+			let timer = setInterval(() => {
+				this.powerUpLifetime -= 0.05;
+				if (this.powerUpLifetime <= 0) clearInterval(timer);
+			}, 50);
+		}
 
 		this.draw = () => {
-			ctx.save();
-			ctx.fillStyle = 'rgba(255, 255, 255, 0.25)';
-			ctx.fillRect(this.x, this.y, this.w, this.h);
-			ctx.fill();
-			ctx.restore();
+			if (this.powerUpLifetime > 0) {
+				let fill = this.w / this.initLifetime * this.powerUpLifetime;
+				ctx.save();
+				ctx.shadowBlur = 15;
+				ctx.shadowColor = '#000';
+				ctx.fillStyle = 'darkred';
+				ctx.fillRect(viewport.w / 2 - fill / 2, viewport.h - this.h, fill, this.h);
+				ctx.restore();
+			}
 		}
 	}
 }
