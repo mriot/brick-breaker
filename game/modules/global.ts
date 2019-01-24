595c697639846eb305256d065a15164c7446d3be
Markus Kremer <mkremer@seibert-media.net>
2019-01-24 22:47:09 +0100

    viewport adjustments

--- game/modules/global.ts
+++ game/modules/global.ts
@@ -1,6 +1,9 @@
 export let canvas: any = document.querySelector('#canvas');
 export let ctx: any = canvas.getContext('2d');
-export let viewport: any = {w: window.innerWidth, h: window.innerHeight};
+export let viewport: any = {
+	w: window.innerWidth < 1000 ? 1000 : window.innerWidth,
+	h: window.innerHeight < 600 ? 600 : window.innerHeight,
+};
 
 export let game: any = {
 	running: false,
